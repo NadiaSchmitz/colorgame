aa52fc9d2d6156875b6c6b383fe1a94dea55530a
NadiaSchmitz <nadiiaschmitz@gmail.com>
2023-01-18 12:05:25 +0100

    First commit

---
+++ color_script.js
@@ -0,0 +1,65 @@
+
+
+var red_range = document.getElementById('red');
+var red_value = red_range.value;
+var red_index = document.getElementById('red_value');
+red_index.innerHTML = red_value;
+
+var green_range = document.getElementById('green');
+var green_value = green_range.value;
+var green_index = document.getElementById('green_value');
+green_index.innerHTML = green_value;
+
+var blue_range = document.getElementById('blue');
+var blue_value = blue_range.value;
+var blue_index = document.getElementById('blue_value');
+blue_index.innerHTML = blue_value;
+
+var color = "rgb(" + red_value + "," + green_value + "," + blue_value +")";
+$('.color_screen').css('background-color', color);
+console.log(color);
+
+function color_range() {
+    red_value = document.getElementById('red').value;
+    red_index = document.getElementById('red_value');
+    red_index.innerHTML = red_value;
+
+    green_value = document.getElementById('green').value;
+    green_index = document.getElementById('green_value');
+    green_index.innerHTML = green_value;
+
+    blue_value = document.getElementById('blue').value;
+    blue_index = document.getElementById('blue_value');
+    blue_index.innerHTML = blue_value;
+    
+    color = "rgb(" + red_value + "," + green_value + "," + blue_value +")";
+    $('.color_screen').css('background-color', color);
+}
+
+function reset() {
+    red_value = 255;
+    green_value = 195;
+    blue_value = 0;
+    red_range.setAttribute('value', red_value);
+    red_index.innerHTML = red_value;
+    green_range.setAttribute('value', green_value);
+    green_index.innerHTML = green_value;
+    blue_range.setAttribute('value', blue_value);
+    blue_index.innerHTML = blue_value;
+    color = "rgb(" + red_value + "," + green_value + "," + blue_value +")";
+    $('.color_screen').css('background-color', color);
+}
+
+function random() {
+    red_value = Math.floor(Math.random() * 255);
+    green_value = Math.floor(Math.random() * 255);
+    blue_value = Math.floor(Math.random() * 255);
+    red_range.setAttribute('value', red_value);
+    red_index.innerHTML = red_value;
+    green_range.setAttribute('value', green_value);
+    green_index.innerHTML = green_value;
+    blue_range.setAttribute('value', blue_value);
+    blue_index.innerHTML = blue_value;
+    color = "rgb(" + red_value + "," + green_value + "," + blue_value +")";
+    $('.color_screen').css('background-color', color);
+}
